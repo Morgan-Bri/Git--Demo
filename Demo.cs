1aa01048d9d7922cde4324fdc3c3f36c87bc001b
MorganBirmingham <1683451388@qq.com>
2025-06-30 10:34:02 +0800

    second save

--- Demo.cs
+++ Demo.cs
@@ -1,5 +1,10 @@
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+    }
+}
+public class Demo2 {
+    public static void main(String[] args) {
+        
     }
 }
